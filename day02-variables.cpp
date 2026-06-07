5400083a02e1c3ef2d69fa928bba0ea86e2f9fa4
akash-yadav-in <akashcodevault@gmail.com>
2026-06-07 20:02:02 +0530

    Add variable demonstration program in C++

---
+++ day02-variables.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    char dev;
+    float m;
+        
+    
+    cout<<"Learning Different Variables"<<endl;
+    cout<<"Write the Value of Salary earned from Company :"<<endl;
+    cin>>a;
+    cout<<"Write the Value of salary earned from Real-estate :"<<endl;
+    cin>>b;
+    cout<<"Write your favourite letter :"<<endl;
+    cin>>dev;
+    cout<<"Write your id number(Format : 12.33): "<<endl;
+    cin>>m;
+    cout<<"Your net salary : "<<(a + b)<<endl<<"Your favourite letter is :  "<<dev<<endl<<"Your id number is : "<<m<<endl;
+      
+
+    
+    return 0;
+}
