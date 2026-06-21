ceb3eb7a7fd652b795dbad5560fee66ac1152176
akash-yadav-in <akashcodevault@gmail.com>
2026-06-21 20:41:06 +0530

    Add C++ program for loop examples
    Demonstrates the use of for, while, and do-while loops in C++.

---
+++ day04-p3 For,while and do while loops.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int a;
+    // USE OF FOR LOOP
+    for(a=1; a<=100; a++){
+        cout<<a<<endl;
+    }
+
+    // USE OF WHILE LOOP
+
+    int m = 1;
+
+    while(m<=40){
+
+        cout<<m<<endl;
+        m++;
+
+    }
+
+    // DO WHILE LOOP
+
+    int v = 1;
+    do{
+        cout<<v<<endl;
+        v++;    
+    }while(v<=10);
+
+     
+
+
+
+
+    return 0; 
+}
