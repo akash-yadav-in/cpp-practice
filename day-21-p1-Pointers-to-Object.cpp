5fb2554157ead5e003de93cf79e9910ed21f82ff
akash-yadav-in <akashcodevault@gmail.com>
2026-08-05 20:02:03 +0530

    Add Complex class with pointer demonstration
    Implemented a Complex class with methods to set and get real and imaginary parts. Demonstrated usage of pointers to access object members.

---
+++ day-21-p1-Pointers-to-Object.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+// WE Studied Pointers To Objects in this lecture
+
+class Complex
+{
+    int real, imaginary;
+
+public:
+    void setData(int a, int b)
+    {
+
+        real = a;
+        imaginary = b;
+    }
+    void getData()
+    {
+        cout << "The real part is " << real << endl;
+        cout << "The imaginary part is " << imaginary << endl;
+    }
+};
+
+int main()
+{
+    // Easy example
+    Complex s;
+
+    // Pointer part
+
+    // Here we have made a pointer p Jisko we have pointed to s
+
+    // We are checking ek pointer se object ke Public memeber ko kaise access kar sakte hain
+    // Complex *p = &s;
+
+    // another way also below
+
+    Complex *p = new Complex;
+
+    // Here we have derefrenced it
+    (*p).setData(2, 3);
+
+    (*p).getData();
+
+    // or
+
+    p->setData(22, 33); // meaninig of -> derefrence the pointer and run that setdate or whichever function present after that arrow
+
+    p->getData();
+
+    return 0;
+}
