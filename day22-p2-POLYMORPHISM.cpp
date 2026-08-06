5033bf2e8bbb71bc7eae07ca084aa1bb8bef5b51
akash-yadav-in <akashcodevault@gmail.com>
2026-08-06 10:10:40 +0530

    Create day22-p2-POLYMORPHISM.cpp for polymorphism demo
    Added a new C++ file demonstrating polymorphism concepts.

---
+++ day22-p2-POLYMORPHISM.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+/*            PolyMorphism ===> ManyForms
+      
+        1. Eg - Function overloading , operator overload
+        2. virtual functioN
+
+                                   POLYMORPHISM 
+                                        |
+                                        |
+                                        |
+       RUNTIME POLYMORPH <----|---------------------|---> COMPILETIME POLYMORPH
+              |                                                  |
+              |---->   Virutal functions                         |----> FUNCTION OVERLOADING
+                                                                 
+                                                                 |----> OPERATOR OVERLOADING
+
+
+*/ 
+
+
+
+int main(){
+   
+return 0;
+}
