584909d7c04dd183e18eddaaadcb5d33cd2dfdd6
akash-yadav-in <akashcodevault@gmail.com>
2026-08-09 16:47:14 +0530

    Implement template theory example in C++
    Added a template theory example with comments.

---
+++ day25-p4-Template-Theory.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+// Templates in C++
+// We Use template as to follow Dry principal protecting of not repeting ouselves
+// we can do genreic programming like one class variable are general to all
+
+/*
+        SAMPLE SYNTAX FOR THE TEMPLATE
+
+        template <class T>    // T can be int, float,char etc
+        class vector {
+                T*arr;      // here arr === array
+                public:
+                -------
+
+
+        };
+
+
+*/
+
+int main(){
+
+     /*
+                     SAMPLE
+
+          vector<int> myvec(ptr);
+          vector<float>myfloating(ptr1);
+          vector<char>mychar(ptr3);
+     
+     
+     
+     
+     */
+   
+return 0;
+}
