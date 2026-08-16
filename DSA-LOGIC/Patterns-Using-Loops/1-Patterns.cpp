2372e9c5b424596035fba5e0d76c7de9a54c489d
akash-yadav-in <akashcodevault@gmail.com>
2026-08-16 21:40:32 +0530

    Add pattern printing program in C++

---
+++ DSA-LOGIC/Patterns-Using-Loops/1-Patterns.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+   int n = 4;
+   for (int i = 1; i <= n; i++)
+   {
+      for (int a = 1; a <= 4; a++)
+      {
+         cout << a;
+      }
+      cout << endl;
+   }
+
+   return 0;
+}
