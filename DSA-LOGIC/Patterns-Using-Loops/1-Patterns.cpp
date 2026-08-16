538354296a11f4ffe1f3f5a2d88e87043324a52d
akash-yadav-in <akashcodevault@gmail.com>
2026-08-16 21:47:28 +0530

    Update 1-Patterns.cpp

--- DSA-LOGIC/Patterns-Using-Loops/1-Patterns.cpp
+++ DSA-LOGIC/Patterns-Using-Loops/1-Patterns.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main()
 {
 
-   int n = 4;
+   int n = 4; // This Thing decides how many vertical 1234 will be printed
    for (int i = 1; i <= n; i++)
    {
-      for (int a = 1; a <= 4; a++)
+      for (int a = 1; a <= 4; a++)  // Here int a decides -> that 1234 will get print once when this loop runs
       {
          cout << a;
       }
