c801b91d0ee304238dc341a9faf4d4f46641ac49
akash-yadav-in <akashcodevault@gmail.com>
2026-08-20 23:00:33 +0530

    Create B-2-Data-Type-modifiers.cpp with explanations
    Added a file explaining data type modifiers in C++.

---
+++ DSA-LOGIC/Bitwise-Operator/B-2-Data-Type-modifiers.cpp
@@ -0,0 +1,12 @@
+/*
+************************DATA TYPE MODIFIERS**************************
+1-> int store 4 bytes that is 32 bits stores
+        but when wewe have to store more than 32 bit we can use modifiers to increse the store data
+      ----- here the values can be diffrent depending on system------
+        1---> long gives >= gives 4bytes  ex long int stores 8 bytes
+        2---> short gives >= decreses size gives 2 bytes space
+        3---> long long => we use with int --> long long int
+        4---> signed => so means that we have int and it is signed means it can store values from -2^31 -1 to 2^31 ans it contains buth positive and negetive value but ehat if we only whant + positive ones
+        5---> unsigned => then we use unsigned if we only wnat to store only positive or only negetive vlaues
+
+*/
