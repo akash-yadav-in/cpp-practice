543fbe10c4eb262f9fb6b82b8cc30de706725d30
akash-yadav-in <akashcodevault@gmail.com>
2026-08-27 19:02:51 +0530

    Create D-12-DSA-Pattern-Problem-04.cpp

---
+++ DSA/D-12-DSA-Pattern-Problem-04.cpp
@@ -0,0 +1,24 @@
+/*
+*************** What to achive in this pattern ********************
+                             1
+                             22
+                             333
+                             4444
+*/
+class Solution
+{
+public:
+    void pattern4(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < i+1; j++)
+            {
+                cout<<i+1;
+            }
+            cout << endl;
+        }
+    }
+};
+
+
