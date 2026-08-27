b608029e1fb86a0bfe798339724981ec3b9d9ccb
akash-yadav-in <akashcodevault@gmail.com>
2026-08-27 19:02:14 +0530

    Create D-11-DSA-Pattern-Problem-03.cpp

---
+++ DSA/D-11-DSA-Pattern-Problem-03.cpp
@@ -0,0 +1,25 @@
+/*
+*************** What to achive in this pattern ********************
+                             1
+                             12
+                             123
+                             1234
+*/
+class Solution
+{
+public:
+    void pattern3(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 1; j <= i + 1; j++)
+            {
+                cout << j;
+            }
+            cout << endl;
+        }
+    }
+};
+
+
+
