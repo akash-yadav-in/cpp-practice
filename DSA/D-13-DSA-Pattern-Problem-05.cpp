00c978eb48a388262113d9540669d69569e312d9
akash-yadav-in <akashcodevault@gmail.com>
2026-08-27 19:03:39 +0530

    Create D-13-DSA-Pattern-Problem-05.cpp

---
+++ DSA/D-13-DSA-Pattern-Problem-05.cpp
@@ -0,0 +1,24 @@
+/*
+*************** What to achive in this pattern ********************
+                             *****
+                             ****
+                             ***
+                             **
+                             *
+*/
+class Solution
+{
+public:
+    void pattern5(int n)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = n; j >i; j--)
+            {
+                cout<<"*";
+            }
+            cout << endl;
+        }
+    }
+};
+
