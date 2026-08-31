5a2a6e99fc5fa9db90420bdaeeb478cc3706530c
akash-yadav-in <akashcodevault@gmail.com>
2026-08-31 07:27:51 +0530

    Create D-29-DSA-Pattern-Problem-21.cpp

---
+++ DSA/D-29-DSA-Pattern-Problem-21.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+using namespace std;
+/*
+*************** What to achive in this pattern ********************
+         
+ 
+                            *            *   -- 8  
+                            **        **    -- 6
+                           ***      ***    -- 4
+                           ****   ****    -- 2
+                           **********    -- 0  End Of Upper Part
+                           ****    ****      -- 2
+                           ***        ***      -- 4
+                           **            **        -- 6
+                           *                *         -- 8
+
+                             
+ 
+*/
+class Solution
+{
+public:
+    void pattern20(int n)
+    {
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int x = 0; x < i+1; x++)
+            {
+                cout<<"*";
+            }
+            for (int s = 2*(n-1) - 2*i; s > 0; s--)
+            {
+                cout<<" ";
+            }
+            for (int x = 0; x < i+1; x++)
+            {
+                cout<<"*";
+            }
+
+            cout<<endl;
+           
+        }
+        for (int i = 0; i < n; i++)
+        {
+           for (int r = (n-1)-i; r > 0; r--)
+           {
+            cout<<"*";
+           }
+           for (int z = 2*(i+1); z > 0; z--)
+           {
+            cout<<" ";
+           }
+           for (int h = (n-1)-i; h > 0; h--)
+           {
+            cout<<"*";
+           }
+           cout<<endl;
+        }
+       
+
+           
+    }
+};
+
+int main()
+{
+    Solution c;
+    c.pattern20(5);
+    return 0;
+}
+
