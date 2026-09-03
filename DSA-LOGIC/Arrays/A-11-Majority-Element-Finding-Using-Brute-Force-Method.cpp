ca755baf39c9af821e3682762dbe03c86c30fabe
akash-yadav-in <akashcodevault@gmail.com>
2026-09-03 05:43:40 +0530

    Fix time complexity comment in majority element function
    Corrected the time complexity comment from O(n^20) to a more reasonable value.

---
+++ DSA-LOGIC/Arrays/A-11-Majority-Element-Finding-Using-Brute-Force-Method.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <vector>
+/*
+          ------------ WHAT TO ACHIEVE ------------
+    So if we are Gievn any array say arr = {1 , 2, 2, 1, 1}
+    and we need to find he max occuring element or Majority element
+
+    1--- Here we have done it using the BRUTE FORCE APPROACH BUT THIS IS NOT BEST As Time Complexity here is O(n^20)
+    
+
+*/
+using namespace std;
+int Majority_Element(vector<int> arr)
+{
+    int size = 5;
+    for (int value : arr)
+    {                      // Here we told the loop for every value of arr you give that element as output --- using this value loop
+        int frequency = 0; // Here we have intialised a frequency to check how many times a number appears
+        for (int elements : arr)
+        { // Main logic here elements is acting same as that of value as of upper loop examples that when the loop run when the value loop runs for first time it will
+          // it will give output as the first element of arr that is 1 from value loop the the element loop run from 1 to last elemt of arr and whenever value == element hoga then it will inc frequncy by 1
+
+            if (elements == value)
+            {
+                frequency++;
+                
+            }
+        }
+        if (frequency > size / 2) // Here agar kisi number ki frequncy total jitne number hain uske addhe se jyada aati hai then wo majority number alredy ho jayega
+        {
+            cout << value;
+        }
+    }
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 2, 1, 1};
+    Majority_Element(arr);
+
+    return 0;
+}
