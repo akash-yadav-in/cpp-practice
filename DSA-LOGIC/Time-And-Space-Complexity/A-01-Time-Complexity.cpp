f7e1736f755e4acdcb4c889be47b99f500199aa4
akash-yadav-in <akashcodevault@gmail.com>
2026-09-07 20:54:23 +0530

    Add time complexity explanation in A-01-Time-Complexity.cpp
    Add comments explaining time complexity.

---
+++ DSA-LOGIC/Time-And-Space-Complexity/A-01-Time-Complexity.cpp
@@ -0,0 +1,6 @@
+/*
+1- Time complexity is amount of time taken in code as how many operations are performent as a input size of (n)----> when increases and decreases.
+   -------> We find time complexity for worst case scenarios
+   -------> Like algrithm we have made to check hot its performing on million and billion of operations
+   -------> And for comparing diffrent algorithm
+*/
