a1b44df58ffff6353540602f3a30b88b663d0678
akash-yadav-in <akashcodevault@gmail.com>
2026-09-08 05:26:13 +0530

    Create A-02-Big-O-Notation.cpp

---
+++ DSA-LOGIC/Time-And-Space-Complexity/A-02-Big-O-Notation.cpp
@@ -0,0 +1,10 @@
+/*
+----------------   2-Big O Notation  -------------------
+-----> Say if the Big (O) notation is O(n)---> so here in worst case scenario only n operations will be performed 
+----->  So if We have a function like 4n^2 + 3n + 5 this for finding the time complexity we have to ignore the constant that is we ignore any mathematical number like here 
+    --> here we ignore 4 and 3 and 5  after then we got n^2 + n + 1 and consider the largest value like agar n change hua to sabse bigger change sabse bada value karega that is n^2
+    --> n^2 ko hum bas mind me rakhenge bakki sabko ignore  
+
+    ---> and so the time complexity will be O(n^2) read as Big of O(n^2)
+
+*/
